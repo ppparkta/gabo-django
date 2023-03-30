487a9b5abea0d01547504b8b508e9d20c0fcc74f
yushinc <cys74351@gmail.com>
2023-03-30 19:15:16 +0900

    0330

--- accountApp/views.py
+++ accountApp/views.py
@@ -1,7 +1,26 @@
-from django.http import HttpResponse
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from accountApp.models import HelloWorld
+
 
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+
+    if request.method == "POST":
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld() #모델 객체 생성
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return HttpResponseRedirect(reverse('accountApp:hello_world'))
+
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountApp/hello_world.html', context={'hello_world_list': hello_world_list})
+
