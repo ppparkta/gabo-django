82c583ee02f5ae6cdc7c342612fa5b9528f9b098
ppparkta <pppppark2@gmail.com>
2023-04-02 01:38:33 +0900

    feat: models, views, ...

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from accountapp.models import HelloWorld
+
+
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse("hello world!")
+    if request.method == "POST":
+        tmp = request.POST.get('hello_world_input')
+        new_hello_world = HelloWorld()
+        new_hello_world.text = tmp
+        new_hello_world.save()
+        return HttpResponseRedirect('/account/hello_world')
 
+    elif request.method == "GET":
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'pragmatic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,6 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
