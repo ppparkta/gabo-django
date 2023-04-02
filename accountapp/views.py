18a409cb058c95cccca6e34986827715866bc5ad
ppparkta <pppppark2@gmail.com>
2023-04-03 01:06:13 +0900

    feat: mypage구현(detail view, 동적 라우팅, context_object_name)

--- accountapp/views.py
+++ accountapp/views.py
@@ -26,3 +26,8 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+class AccountDetailView(DeleteView):
+    model = User
+    context_object_name = 'target_user' #템플릿에서 사용하는 user객체의 이름을 다르게 설정할 수 있음
+    template_name = 'accountapp/detail.html'
