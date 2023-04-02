786096505c06466f56a09cbea9c84a2e92103402
ppparkta <pppppark2@gmail.com>
2023-04-02 23:52:24 +0900

    feat: 회원가입, 로그인, 로그아웃 구현(CreateView, LoginView, LogoutView)

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 from accountapp.models import HelloWorld
 
@@ -12,8 +16,13 @@ def hello_world(request):
         new_hello_world = HelloWorld()
         new_hello_world.text = tmp
         new_hello_world.save()
-        return HttpResponseRedirect('/account/hello_world')
-
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     elif request.method == "GET":
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
