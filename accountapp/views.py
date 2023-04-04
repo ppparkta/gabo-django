d2820a81a0b8d9ce147f7c7136472b7fb9205e6d
ppparkta <pppppark2@gmail.com>
2023-04-04 23:13:15 +0900

    fix: accountapp url 변경으로 인한 코드 수정

--- accountapp/views.py
+++ accountapp/views.py
@@ -12,15 +12,18 @@ from accountapp.models import HelloWorld
 # Create your views here.
 
 def hello_world(request):
-    if request.method == "POST":
-        tmp = request.POST.get('hello_world_input')
-        new_hello_world = HelloWorld()
-        new_hello_world.text = tmp
-        new_hello_world.save()
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    elif request.method == "GET":
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            tmp = request.POST.get('hello_world_input')
+            new_hello_world = HelloWorld()
+            new_hello_world.text = tmp
+            new_hello_world.save()
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        elif request.method == "GET":
+            hello_world_list = HelloWorld.objects.all()
+            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+    else:
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 class AccountCreateView(CreateView):
     model = User
