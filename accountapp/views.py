cfad707226d11376bad6be3cde9f96b26deda276
ppparkta <pppppark2@gmail.com>
2023-04-04 21:46:24 +0900

    fix: target_user 설정

--- accountapp/views.py
+++ accountapp/views.py
@@ -35,11 +35,13 @@ class AccountDetailView(DeleteView):
 
 class AccountUpdateView(UpdateView):
     model = User
+    context_object_name = 'target_user'
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
 class AccountDeleteView(DeleteView):
     model = User
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
