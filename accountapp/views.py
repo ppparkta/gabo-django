8e3abe73e96dc5b05ebfb1a6d0f07df6fc147943
ppparkta <pppppark2@gmail.com>
2023-04-06 13:40:34 +0900

    refactor: decorator 이용해서 사용자 인증 관리

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,31 +1,30 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
-
+has_ownership = [account_ownership_required, login_required]
 # Create your views here.
 
+@login_required
 def hello_world(request):
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('accountapp:login'))
-    else:
-        if request.method == "POST":
-            tmp = request.POST.get('hello_world_input')
-            new_hello_world = HelloWorld()
-            new_hello_world.text = tmp
-            new_hello_world.save()
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        elif request.method == "GET":
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-
-
+    if request.method == "POST":
+        tmp = request.POST.get('hello_world_input')
+        new_hello_world = HelloWorld()
+        new_hello_world.text = tmp
+        new_hello_world.save()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+    elif request.method == "GET":
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 class AccountCreateView(CreateView):
     model = User
@@ -38,6 +37,8 @@ class AccountDetailView(DeleteView):
     context_object_name = 'target_user' #템플릿에서 사용하는 user객체의 이름을 다르게 설정할 수 있음
     template_name = 'accountapp/detail.html'
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
@@ -45,32 +46,10 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().get(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
-
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().get(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
