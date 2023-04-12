20bff8899d3ecf30666e182aa814daa9977aea45
ppparkta <pppppark2@gmail.com>
2023-04-13 00:31:05 +0900

    fix: todo_detail 들어갈때 생기는 오류 수정, accountapp과 todo 연결

--- todo/views.py
+++ todo/views.py
@@ -7,9 +7,11 @@ def todo_list(request):
     todos = Todo.objects.filter(complete=False)
     return render(request, 'todo/todo_list.html', {'todos': todos})
 
+
 def todo_detail(request, pk):
     todo = Todo.objects.get(id=pk)
     return render(request, 'todo/todo_detail.html', {'todo': todo})
+
 
 def todo_post(request):
     if request.method == "POST":
@@ -17,10 +19,23 @@ def todo_post(request):
         if form.is_valid():
             todo = form.save(commit=False)
             todo.save()
-            return redirect('/todos/')
+            return redirect('todo:todo_list')
     else:
         form = TodoForm()
     return render(request, 'todo/todo_post.html', {'form': form})
+
+
+def done_list(request):
+    dones = Todo.objects.filter(complete=True)
+    return render(request, 'todo/done_list.html', {'dones': dones})
+
+
+def todo_done(request, pk):
+    todo = Todo.objects.get(id=pk)
+    todo.complete = True
+    todo.save()
+    return redirect('todo:todo_list')
+
 
 def todo_edit(request, pk):
     todo = Todo.objects.get(id=pk)
@@ -29,17 +44,7 @@ def todo_edit(request, pk):
         if form.is_valid():
             todo = form.save(commit=False)
             todo.save()
-            return redirect('/todos/')
+            return redirect('todo:todo_list')
     else:
         form = TodoForm(instance=todo)
     return render(request, 'todo/todo_post.html', {'form': form})
-
-def done_list(request):
-    todos = Todo.objects.filter(complete=True)
-    return render(request, 'todo/todo_list.html', {'todos': todos})
-
-def todo_done(request, pk):
-    todo = Todo.objects.get(id=pk)
-    todo.complete = True
-    todo.save()
-    return redirect('/todos/')
