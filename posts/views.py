076c3d534d09a7974687743efebe8c91e3f3e691
ppparkta <pppppark2@gmail.com>
2023-05-25 15:24:23 +0900

    feat: comment 기능 추가 및 post에 연결

--- posts/views.py
+++ posts/views.py
@@ -7,9 +7,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from users.models import Profile
-from .models import Post
+from .models import Post, Comment
 from .permissions import CustomReadOnly
-from .serializers import PostSerializer, PostCreateSerializer
+from .serializers import PostSerializer, PostCreateSerializer, CommentSerializer, CommentCreateSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -40,15 +40,15 @@ def like_post(request, pk):
     return Response({'status': 'ok'})
 
 
-# class CommentViewSet(viewsets.ModelViewSet):
-#     queryset = Comment.objects.all()
-#     permission_classes = [CustomReadOnly]
-#
-#     def get_serializer_class(self):
-#         if self.action == 'list' or 'retrieve':
-#             return CommentSerializer
-#         return CommentCreateSerializer
-#
-#     def perform_create(self, serializer):
-#         profile = Profile.objects.get(user=self.request.user)
-#         serializer.save(author=self.request.user, profile=profile)
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    permission_classes = [CustomReadOnly]
+
+    def get_serializer_class(self):
+        if self.action == 'list' or 'retrieve':
+            return CommentSerializer
+        return CommentCreateSerializer
+
+    def perform_create(self, serializer):
+        profile = Profile.objects.get(user=self.request.user)
+        serializer.save(author=self.request.user, profile=profile)
